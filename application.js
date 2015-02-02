94fc7832a91d9badfac4e0ed82d825f7a274e785
Jameson Knoll <j.knoll621@gmail.com>
2015-02-03 01:32:57 +0800

    update readme

--- application.js
+++ application.js
@@ -2,11 +2,11 @@ if (navigator.geolocation) {
 navigator.geolocation.getCurrentPosition(success,error);
 }
 else {
-alert('Geolocation is not supported');
+alert('Error: Geolocation unsupported');
 }
 
 function error() {
-alert("That's weird! We couldn't find you!");
+alert("We're having issues locating you. Have you disabled geolocation in your browser? Or are you on a local server? Please see README for more info.");
 }
 
 function success(position) {
@@ -14,19 +14,19 @@ function success(position) {
       var apikey = "10cfa1d790a05aa4"
       var weatherForecast = "http://api.wunderground.com/api/"+ apikey +"/forecast/geolookup/conditions/q/" + position.coords.latitude + "," + position.coords.longitude + ".json";
 
+      //get data from api and set relevant info to variables
       $.getJSON( weatherForecast, function( data ) {
       		var location =data['location']['city'];
-			var temp = data['current_observation']['temp_f'];
+			var temperature = data['current_observation']['temp_f'];
 			var img = data['current_observation']['icon_url'];
 			var desc = data['current_observation']['weather'];
 			var wind = data['current_observation']['wind_string'];
 
-						//setting the spans to the correct parameters
+			//jQuery apply data to page <div>s
 			$('#location').html(location);
-			$('#temp').html(temp);
+			$('#temp').html(temperature);
 			$('#desc').html(desc);
 			$('#wind').html(wind);
-			//filling the image src attribute with the image url
 			$('#img').attr('src', img);
 		});
 }
